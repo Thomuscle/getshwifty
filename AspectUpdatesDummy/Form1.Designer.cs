cad56af48f631c5b4a2b89e8d6c74946bc73a658
Thomuscle <jtho654@aucklanduni.ac.nz>
2015-12-01 11:36:53 +1300

    Created main menu

--- AspectUpdatesDummy/Form1.Designer.cs
+++ AspectUpdatesDummy/Form1.Designer.cs
@@ -28,20 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainMenuTitle = new System.Windows.Forms.Label();
+            this.mainAddCustBtn = new System.Windows.Forms.Button();
+            this.mainAddVersBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // mainMenuTitle
+            // 
+            this.mainMenuTitle.AutoSize = true;
+            this.mainMenuTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainMenuTitle.Location = new System.Drawing.Point(121, 9);
+            this.mainMenuTitle.Name = "mainMenuTitle";
+            this.mainMenuTitle.Size = new System.Drawing.Size(276, 24);
+            this.mainMenuTitle.TabIndex = 0;
+            this.mainMenuTitle.Text = "Aspect PT Version Database";
+            // 
+            // mainAddCustBtn
+            // 
+            this.mainAddCustBtn.Location = new System.Drawing.Point(163, 86);
+            this.mainAddCustBtn.Name = "mainAddCustBtn";
+            this.mainAddCustBtn.Size = new System.Drawing.Size(191, 46);
+            this.mainAddCustBtn.TabIndex = 1;
+            this.mainAddCustBtn.Text = "Add Customer";
+            this.mainAddCustBtn.UseVisualStyleBackColor = true;
+            this.mainAddCustBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainAddVersBtn
+            // 
+            this.mainAddVersBtn.Location = new System.Drawing.Point(163, 177);
+            this.mainAddVersBtn.Name = "mainAddVersBtn";
+            this.mainAddVersBtn.Size = new System.Drawing.Size(191, 46);
+            this.mainAddVersBtn.TabIndex = 2;
+            this.mainAddVersBtn.Text = "Add Version";
+            this.mainAddVersBtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(601, 345);
+            this.ClientSize = new System.Drawing.Size(526, 278);
+            this.Controls.Add(this.mainAddVersBtn);
+            this.Controls.Add(this.mainAddCustBtn);
+            this.Controls.Add(this.mainMenuTitle);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label mainMenuTitle;
+        private System.Windows.Forms.Button mainAddCustBtn;
+        private System.Windows.Forms.Button mainAddVersBtn;
     }
 }
 
